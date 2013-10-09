41a32170c444080bf1dcf4d42a50353372e2ddb9
Igor Galić <i.galic@brainsware.org>
2013-10-09 02:29:49 +0000

    add a config for puppet agent bootstraps
    This file is wget-ed in the late_command and makes it easily possible to
    bootstrap puppet

--- manifests/install/cobbler.pp
+++ manifests/install/cobbler.pp
@@ -43,15 +43,19 @@ class kvmhost::install::cobbler {
     server_ip      => '192.168.122.1',
     next_server_ip => '192.168.122.1',
     allow_access   => '192.168.122.1 127.0.0.1',
+    defaultrootpw  => '$6$JdSUnsX8THgmQS4$U.djCmthXU7v9DbxQLLOYbicNOa8Yy1oroFj2RDjQu/RdwVI0lXN10.48qbFNRLrBiV41vKOFu2AFf4iBUQ0t1',
   }
 
   file { '/srv/www/cobbler/ks_mirror/config/internal.cfg':
     content => template('kvmhost/internal.cfg.erb'),
     require => Class[::cobbler],
   }
-
   file { '/srv/www/cobbler/ks_mirror/config/external.cfg':
     content => template('kvmhost/external.cfg.erb'),
+    require => Class[::cobbler],
+  }
+  file { '/srv/www/cobbler/ks_mirror/config/puppet.conf':
+    source  => 'puppet:///modules/kvmhost/puppet.conf',
     require => Class[::cobbler],
   }
 }
