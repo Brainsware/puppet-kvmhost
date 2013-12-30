9035d2d0d0b24752de1f5b1f2712919dbc5c5193
Alexander Pánek <a.panek@brainsware.org>
2013-12-30 19:15:18 +0000

    Implemented cobblersystems_to_unbound
    Takes the cobblersystems hash in <host>.esat.yml and returns a hash of the form:
    { 'host' => { 'ip' => <IP>, 'ipv6' => <IPV6> } }

--- lib/puppet/parser/functions/cobblersystems_to_unbound.rb
+++ lib/puppet/parser/functions/cobblersystems_to_unbound.rb
@@ -15,15 +15,24 @@ module Puppet::Parser::Functions
             "given #{arguments.size} for 1.")
     end
 
-    collection = arguments[0]
+    input = arguments[0]
+    output = {}
+
+    get_ip = { |data, key| data['interfaces']['eth0'][key] }
 
     raise(TypeError, "cobblersystems_to_unbound(): first argument must be a Hash. " +
           "Given an argument of class #{collection.class}.") unless collection.is_a? Hash
 
-    collection.each do |item|
+    input.each do |host, data|
+      hostname = data['hostname'] || host
 
+      output[hostname] = {
+        'ip'   => get_ip.call[data, 'ip_address'],
+        'ipv6' => get_ip.call[data, 'ipv6_address']
+      }
     end
-    collection
+
+    output
   end
 end
 
