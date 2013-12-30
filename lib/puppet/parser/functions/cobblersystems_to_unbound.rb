5d7406ed02ab09bdd58766c88e4e59fea05474b6
Alexander Pánek <a.panek@brainsware.org>
2013-12-30 19:00:49 +0000

    Renamed cobblers2unbound to cobblersystems_to_unbound

--- lib/puppet/parser/functions/cobblersystems_to_unbound.rb
+++ lib/puppet/parser/functions/cobblersystems_to_unbound.rb
@@ -5,19 +5,19 @@
 # takes an Hash of cobblersystems and transforms them to a Hash of unbound record hashes
 
 module Puppet::Parser::Functions
-  newfunction(:cobblers2unbound, :type => :rvalue, :doc => <<-EOS
+  newfunction(:cobblersystems_to_unbound, :type => :rvalue, :doc => <<-EOS
   takes an Hash of cobblersystems and transforms them to a Hash of unbound record hashes
   EOS
   ) do |arguments|
 
     if (arguments.size != 1) then
-      raise(Puppet::ParseError, "cobblers2unbound(): Wrong number of arguments "+
+      raise(Puppet::ParseError, "cobblersystems_to_unbound(): Wrong number of arguments "+
             "given #{arguments.size} for 1.")
     end
 
     collection = arguments[0]
 
-    raise(TypeError, "cobblers2unbound(): first argument must be a Hash. " +
+    raise(TypeError, "cobblersystems_to_unbound(): first argument must be a Hash. " +
           "Given an argument of class #{collection.class}.") unless collection.is_a? Hash
 
     collection.each do |item|
