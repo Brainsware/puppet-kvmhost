a8d9314266be61721aaec0c33a32709139a5f6c3
Igor Galić <i.galic@brainsware.org>
2014-09-12 12:51:58 +0200

    rip out all networking

--- manifests/init.pp
+++ manifests/init.pp
@@ -32,46 +32,13 @@
 # Copyright 2014 Brainsware
 #
 class kvmhost (
-  $ip               = undef,
-  $broadcast        = undef,
-  $netmask          = undef,
-  $gateway          = undef,
-  $network          = undef,
-  $bridge           = 'virbr0',
-  $interface        = 'eth0',
-  $ipv6             = undef,
-  $ipv6_gateway     = undef,
   $defaultrootpw    = '',
   $root_key_private = undef,
   $root_key_public  = undef,
 ){
-  unless is_ip_address($ip) {
-    fail("${module_name}: ip must be a valid IP address: ${ip}")
-  }
-  unless is_ip_address($broadcast) {
-    fail("${module_name}: broadcast must be a valid IP address: ${broadcast}")
-  }
-  unless is_ip_address($netmask) {
-    fail("${module_name}: netmask must be a valid IP address: ${netmask}")
-  }
-  unless is_ip_address($network) {
-    fail("${module_name}: network must be a valid IP address: ${network}")
-  }
-  unless is_ip_address($gateway) {
-    fail("${module_name}: gateway must be a valid IP address: ${gateway}")
-  }
-  unless is_ip_address("${ipv6}::1") {
-    fail("${module_name}: IPv6 must be a valid IPv6 address: ${ipv6}::1")
-  }
-  unless is_ip_address($ipv6_gateway) {
-    fail("${module_name}: IPv6 Gateway must be a valid IPv6 address: ${ipv6_gateway}")
-  }
+  contain '::kvmhost::libvirt'
+  contain '::kvmhost::cobbler'
 
-  contain 'kvmhost::network'
-  contain 'kvmhost::install'
-  contain 'kvmhost::config'
-
-  Class['kvmhost::network'] ->
-  Class['kvmhost::install'] ->
-  Class['kvmhost::config']
+  Class['::kvmhost::libvirt'] ->
+  Class['::kvmhost::cobbler']
 }
